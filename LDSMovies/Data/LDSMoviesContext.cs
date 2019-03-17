becf69c327a30157a3eed26ffc8750ce69e0e68d
gpont1 <35281959+gpont1@users.noreply.github.com>
2019-03-16 21:41:30 -0600

    Initial Push

---
+++ LDSMovies/Data/LDSMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LDSMovies.Models;
+
+namespace LDSMovies.Models
+{
+    public class LDSMoviesContext : DbContext
+    {
+        public LDSMoviesContext (DbContextOptions<LDSMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LDSMovies.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ LDSMovies/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using LDSMovies.Models;
+
+
+namespace LDSMovies.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LDSMoviesContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<LDSMoviesContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2002-04-12"),
+                        Genre = "Missionary Drama",
+                        Price = 7.99M,
+                        Rating = "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Cokeville Miracle ",
+                        ReleaseDate = DateTime.Parse("2015-06-05"),
+                        Genre = "Drama",
+                        Price = 8.99M,
+                        Rating = "PG-13"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The R.M.",
+                        ReleaseDate = DateTime.Parse("2003-01-31"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "PG"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Singles Ward",
+                        ReleaseDate = DateTime.Parse("2002-01-30"),
+                        Genre = "Comedy",
+                        Price = 3.99M,
+                        Rating = "PG"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
